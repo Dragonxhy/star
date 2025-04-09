e145046268cb03eb194704b351b6a040e29f6a0a
xhy <1366580217@qq.com>
2025-04-09 15:30:21 +0800

    初次提交

---
+++ cesium-basic/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import * as Cesium from 'cesium';
+import "./Widgets/widgets.css";
+import {onMounted} from "vue";
+
+//设置cesium静态资源路径
+window.CESIUM_BASE_URL = "/";
+
+onMounted(()=>{
+  var viewer=new Cesium.Viewer("cesiumContainer");
+});
+</script>
+
+<template>
+<div id="cesiumContainer" ref="cesiumContainer"></div>
+</template>
+
+<style scoped>
+* {
+margin: 0;
+padding: 0;
+}
+#cesiumContainer {
+width: 100vw;
+height: 100vh;
+}
+</style>
